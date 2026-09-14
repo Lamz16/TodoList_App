743b969cc597d898ea07960d32a55d69183c488e
Andi Salam Syahputra <97329955+Lamz16@users.noreply.github.com>
2026-09-14 09:58:37 +0700

    Fix archive text contrast for nature theme

--- app/src/main/java/com/lamz/todolistapp/ui/completed/CompletedFragment.kt
+++ app/src/main/java/com/lamz/todolistapp/ui/completed/CompletedFragment.kt
@@ -5,13 +5,11 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import androidx.core.content.ContextCompat
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.lifecycleScope
 import androidx.recyclerview.widget.LinearLayoutManager
 import com.google.firebase.auth.FirebaseAuth
 import com.lamz.todolistapp.DetailActivity
-import com.lamz.todolistapp.R
 import com.lamz.todolistapp.TodoAdapter
 import com.lamz.todolistapp.data.TodoItem
 import com.lamz.todolistapp.databinding.FragmentCompletedBinding
@@ -36,26 +34,17 @@ class CompletedFragment : Fragment() {
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
-
         auth = FirebaseAuth.getInstance()
-
-        val colorTeks = ContextCompat.getColor(requireContext(), R.color.white)
-        binding.text1.setTextColor(colorTeks)
 
         lifecycleScope.launch { completedViewModel.fetchTodoList() }
         completedViewModel.todoList.observe(viewLifecycleOwner) { todoList ->
             val emptyBanner = binding.emptyBanner.root
             emptyBanner.visibility = if (todoList.isNotEmpty()) View.INVISIBLE else View.VISIBLE
-            if (todoList.isNotEmpty()) {
-                updateUI(todoList)
-            }
+            if (todoList.isNotEmpty()) updateUI(todoList)
         }
 
-        val loadingProgressBar = binding.loadingProgressBar
-
         completedViewModel.isLoading.observe(viewLifecycleOwner) { isLoading ->
-
-            loadingProgressBar.visibility = if (isLoading) View.VISIBLE else View.GONE
+            binding.loadingProgressBar.visibility = if (isLoading) View.VISIBLE else View.GONE
         }
 
         binding.rvTodo.apply {
@@ -65,13 +54,8 @@ class CompletedFragment : Fragment() {
     }
 
     private fun updateUI(todoList: ArrayList<TodoItem>) {
-
         val adapter = TodoAdapter(todoList)
-
-
         binding.rvTodo.adapter = adapter
-
-
         adapter.setOnItemClickListener(object : TodoAdapter.OnItemClickListener {
             override fun onItemClick(position: Int) {
                 val clickedItem = todoList[position]
@@ -83,11 +67,9 @@ class CompletedFragment : Fragment() {
             override fun onCheckboxChanged(position: Int, isChecked: Boolean) {
                 val uidCompleted = FirebaseAuth.getInstance().currentUser?.uid
                 val clickedItem = todoList[position]
-                val todoRef =
-                    Utils.firebaseDatabaseTodo.child(clickedItem.todoId)
+                val todoRef = Utils.firebaseDatabaseTodo.child(clickedItem.todoId)
                 todoRef.child(Utils.COMPLETED).setValue(if (isChecked) "yes" else "no")
-                todoRef.child(Utils.UID_COMPLETED)
-                    .setValue(if (isChecked) "${uidCompleted}_yes" else "")
+                todoRef.child(Utils.UID_COMPLETED).setValue(if (isChecked) "${uidCompleted}_yes" else "")
                 todoRef.child(Utils.STATUS).setValue(if (isChecked) "Complete" else "Incomplete")
             }
         })
